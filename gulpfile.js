75fb23ada3a15b4bb37e3eea671e97a129d4093a
joshtowers <joshtowers@gmail.com>
2015-03-03 19:17:35 -0600

    update dependencies to latest version and re-format some basic js

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,36 @@
-var gulp = require('gulp');
-var browserify = require('gulp-browserify');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var fileinclude = require('gulp-file-include')
+var gulp        = require('gulp');
+var browserify  = require('gulp-browserify');
+var concat      = require('gulp-concat');
+var sass        = require('gulp-sass');
+var fileinclude = require('gulp-file-include');
 
 gulp.task('browserify', function() {
     gulp.src('src/js/main.js')
       .pipe(browserify({transform: 'reactify'}))
       .pipe(concat('main.js'))
       .pipe(gulp.dest('dist/js'));
-
 });
-
-// gulp.task('copy', function() {
-//     gulp.src('src/index.html')
-//       .pipe(gulp.dest('dist'));
-// });
-
 
 gulp.task('copy-markup', function() {
   gulp.src('src/**/*.html')
-  .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('copy-assets', function() {
   gulp.src('src/img/**/*.*')
-  .pipe(gulp.dest('dist/img'));
+    .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('copy-html', function() {
-    gulp.src('html/*.html')
-      .pipe(gulp.dest('dist-html'));
+  gulp.src('html/*.html')
+    .pipe(gulp.dest('dist-html'));
 });
 
 gulp.task('sass', function () {
-    gulp.src('src/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/css'))
-        .pipe(gulp.dest('dist-html/css'));
+  gulp.src('src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(gulp.dest('dist-html/css'));
 });
 
 gulp.task('fileinclude', function() {
@@ -50,13 +43,7 @@ gulp.task('fileinclude', function() {
 });
 
 gulp.task('html', ['copy-html']);
-
 gulp.task('default',['browserify', 'copy-markup', 'copy-assets', 'sass', 'fileinclude']);
-//gulp.task('default',['copy', 'sass', 'html']);
-
 gulp.task('watch', function() {
-
     gulp.watch(['src/**/*.*', 'html/**/*.*'], ['default']);
-
-    
 });
