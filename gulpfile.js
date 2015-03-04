b8c2b3693e272617b4f1dc48e4d1104761321041
joshtowers <joshtowers@gmail.com>
2015-03-03 19:35:05 -0600

    just js formatting

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,45 @@
-var gulp        = require('gulp');
-var browserify  = require('gulp-browserify');
-var concat      = require('gulp-concat');
-var sass        = require('gulp-sass');
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
 var fileinclude = require('gulp-file-include');
 
 gulp.task('browserify', function() {
-    gulp.src('src/js/main.js')
-      .pipe(browserify({transform: 'reactify'}))
-      .pipe(concat('main.js'))
-      .pipe(gulp.dest('dist/js'));
+	gulp.src('src/js/main.js')
+		.pipe(browserify({
+			transform: 'reactify'
+		}))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('copy-markup', function() {
-  gulp.src('src/**/*.html')
-    .pipe(gulp.dest('dist'));
+	gulp.src('src/**/*.html')
+		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('copy-assets', function() {
-  gulp.src('src/img/**/*.*')
-    .pipe(gulp.dest('dist/img'));
+	gulp.src('src/img/**/*.*')
+		.pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('sass', function () {
-  gulp.src('src/scss/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('dist/css'))
-    .pipe(gulp.dest('dist-html/css'));
+gulp.task('sass', function() {
+	gulp.src('src/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('dist/css'))
+		.pipe(gulp.dest('dist-html/css'));
 });
 
 gulp.task('fileinclude', function() {
-  gulp.src(['html/*.html'])
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: 'html/partials/'
-    }))
-    .pipe(gulp.dest('dist-html'));
+	gulp.src(['html/*.html'])
+		.pipe(fileinclude({
+			prefix: '@@',
+			basepath: 'html/partials/'
+		}))
+		.pipe(gulp.dest('dist-html'));
 });
 
-gulp.task('default',['browserify', 'copy-markup', 'copy-assets', 'sass', 'fileinclude']);
+gulp.task('default', ['browserify', 'copy-markup', 'copy-assets', 'sass', 'fileinclude']);
 gulp.task('watch', function() {
-    gulp.watch(['src/**/*.*', 'html/**/*.*'], ['default']);
+	gulp.watch(['src/**/*.*', 'html/**/*.*'], ['default']);
 });
--- src/js/stores/app-data.js
+++ src/js/stores/app-data.js
@@ -1,53 +1,41 @@
-
 var AppData = [{
-    'DesignName': 'FordUnity',
-    'DisplayName':'Ford Unity',
-    'Layouts': ['A', 'B', 'C', 'D'],
-    'Palettes': [
-        {
-            'paletteName': 'EsapeBlueDark',
-            'displayName': 'Escape Blue Dark',
-            'primaryColor': '#333'
-        },
-        {
-            'paletteName': 'EsapeBlueLight',
-            'displayName': 'Escape Blue Light',
-            'primaryColor': '#333'
-        }
-    ]
-  },
-  {
-    'DesignName': 'FordMagnetic',
-    'DisplayName':'Ford Magnetic',
-    'Layouts': ['A', 'B', 'C', 'D'],
-    'Palettes': [
-        {
-            'paletteName': 'EsapeBlueDark',
-            'displayName': 'Escape Blue Dark',
-            'primaryColor': '#333'
-        },
-        {
-            'paletteName': 'EsapeBlueLight',
-            'displayName': 'Escape Blue Light',
-            'primaryColor': '#333'
-        }
-    ]
-  },
-  {
-    'DesignName': 'FordPremier',
-    'DisplayName':'Ford Premier',
-    'Layouts': ['A', 'B', 'C', 'D'],
-    'Palettes': [
-        {
-            'paletteName': 'EsapeBlueDark',
-            'displayName': 'Escape Blue Dark',
-            'primaryColor': '#333'
-        },
-        {
-            'paletteName': 'EsapeBlueLight',
-            'displayName': 'Escape Blue Light',
-            'primaryColor': '#333'
-        }
-    ]
-  }];
+	'DesignName': 'FordUnity',
+	'DisplayName': 'Ford Unity',
+	'Layouts': ['A', 'B', 'C', 'D'],
+	'Palettes': [{
+		'paletteName': 'EsapeBlueDark',
+		'displayName': 'Escape Blue Dark',
+		'primaryColor': '#333'
+	}, {
+		'paletteName': 'EsapeBlueLight',
+		'displayName': 'Escape Blue Light',
+		'primaryColor': '#333'
+	}]
+}, {
+	'DesignName': 'FordMagnetic',
+	'DisplayName': 'Ford Magnetic',
+	'Layouts': ['A', 'B', 'C', 'D'],
+	'Palettes': [{
+		'paletteName': 'EsapeBlueDark',
+		'displayName': 'Escape Blue Dark',
+		'primaryColor': '#333'
+	}, {
+		'paletteName': 'EsapeBlueLight',
+		'displayName': 'Escape Blue Light',
+		'primaryColor': '#333'
+	}]
+}, {
+	'DesignName': 'FordPremier',
+	'DisplayName': 'Ford Premier',
+	'Layouts': ['A', 'B', 'C', 'D'],
+	'Palettes': [{
+		'paletteName': 'EsapeBlueDark',
+		'displayName': 'Escape Blue Dark',
+		'primaryColor': '#333'
+	}, {
+		'paletteName': 'EsapeBlueLight',
+		'displayName': 'Escape Blue Light',
+		'primaryColor': '#333'
+	}]
+}];
 module.exports = AppData;
